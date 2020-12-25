5522f506eb2c67afbe28744c067ab12a3ef7a6ac
shankar <shankar@hanadockers1.jamb.io>
2020-12-24 19:58:17 -0500

    PHP odbc test connectivity

---
+++ dbHANA.php
@@ -0,0 +1,10 @@
+<?php
+$conn = new PDO("odbc:DRIVER={/var/www/html/hdbclient/libodbcHDB.so};Servernode=hanadockers1e1.jamb.io:39013;DATABASENAME=HXE;UID=SYSTEM;PWD=HanaDocker1@");
+$conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+$conn->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
+$query = "SELECT * FROM dummy";
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$product = $stmt->fetch();
+var_dump($product);
+
